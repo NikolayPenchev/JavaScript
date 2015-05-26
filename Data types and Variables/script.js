cd32bdf0ac6325df772c84c84b0e5a0530a12d03
Martin Krustev <mr.krustev1994@gmail.com>
2015-05-27 01:25:55 +0300

    JavaScript Homework

--- Data types and Variables/script.js
+++ Data types and Variables/script.js
@@ -1,46 +1,52 @@
-var justString = "Chew, baka!"
-var randomNumber = 4; // It's a joke from one of Evlogi's lectures. Never make this kind of variable!
-var isTrue = true;
+var justString = "Chew, baka!",
+    randomNumber = 4,
+    randomFloat = 4.5,
+    myArray = ['empty','array'],
+    myObject = {},// It's a joke from one of Evlogi's lectures. Never make this kind of variable!
+    isTrue = true,
+    joeyPickUp = '\'How you doin\'?\', Joey said.',
+    imNull = null,
+    imUndefined;
 
-function solveFirst(){
-    document.getElementById("outputOne").innerHTML = justString + "<br>" + randomNumber + "<br>" + isTrue;
+function solveFirst() {
+    document.getElementById("outputOne").innerHTML = justString + '<br>' + randomNumber + '<br>' + randomFloat +
+        '<br>' + myArray + '<br>' + myObject + '<br>' + isTrue;
+
 };
 // Very noobish approach but still works.
-function hideFirst(){
+function hideFirst() {
     document.getElementById("outputOne").innerHTML = "";
 }
 
-var joeyPickUp = '\'How you doin\'?\', Joey said.';
-
-function solveSecond(){
+function solveSecond() {
     document.getElementById("outputTwo").innerHTML = joeyPickUp;
 };
 
-function hideSecond(){
+function hideSecond() {
     document.getElementById("outputTwo").innerHTML = "";
 }
 
-function solveThird(){
-    document.getElementById("outputThree").innerHTML = justString + ' has typeof: ' + typeof justString + "<br>" +
-        randomNumber + ' has typeof: ' + typeof randomNumber + "<br>" + isTrue + ' has typeof: ' + typeof isTrue;
+function solveThird() {
+    document.getElementById("outputThree").innerHTML = justString + ' has typeof: ' + typeof justString + '<br>' +
+        randomNumber + ' has typeof: ' + typeof randomNumber + '<br>' + randomFloat + ' has typeof: ' + typeof randomFloat + '<br>' +
+        myArray + ' has typeof: ' + typeof myArray + '<br>' + myObject + ' has typeof: ' + typeof myObject + '<br>'
+        + isTrue + ' has typeof: ' + typeof isTrue;
 };
 
-function hideThird(){
+function hideThird() {
     document.getElementById("outputThree").innerHTML = "";
 }
 
-var imNull = null;
-var imUndefined;
 
-function solveFourth(){
-    document.getElementById("outputFour").innerHTML = imNull + ' has typeof: ' + typeof imNull + "<br>" +
+function solveFourth() {
+    document.getElementById("outputFour").innerHTML = imNull + ' has typeof: ' + typeof imNull + '<br>' +
         imUndefined + ' has typeof: ' + typeof imUndefined;
 };
 
-function hideFourth(){
+function hideFourth() {
     document.getElementById("outputFour").innerHTML = "";
 }
 
-function superSecretStuff(){
+function superSecretStuff() {
     window.open("https://www.youtube.com/watch?v=Xu3FTEmN-eg")
 }
